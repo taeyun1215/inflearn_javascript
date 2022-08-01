25575390eaaf595f54f000ba0d22215513e1625a
taeyun1215 <taeyun1215@naver.com>
2022-08-01 22:03:25 +0900

    create user

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://taeyun:woogi101^^@boilerplate.vsywumc.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true //useCreateIndex: true, useFindAndModify: false
+  }).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
