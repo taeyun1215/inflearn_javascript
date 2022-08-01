fd1b53eefd3d9c9167d5699f55561e2c9baff5f1
taeyun1215 <taeyun1215@naver.com>
2022-08-01 22:39:51 +0900

    create User API

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose.connect('mongodb+srv://taeyun:woogi101^^@boilerplate.vsywumc.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true //useCreateIndex: true, useFindAndModify: false
@@ -11,6 +17,17 @@ mongoose.connect('mongodb+srv://taeyun:woogi101^^@boilerplate.vsywumc.mongodb.ne
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) =>  {
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
