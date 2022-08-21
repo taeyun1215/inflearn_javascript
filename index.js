eafce509f0851392b7681de28935ea80ac6698ff
taeyun1215 <taeyun1215@naver.com>
2022-08-21 22:43:05 +0900

    error correction

--- index.js
+++ index.js
@@ -77,6 +77,17 @@ app.post('/login', (req, res) => {
 
 app.get ('/api/users/auth', auth, (req, res) => {
 
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -74,10 +74,9 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
-userSchema.methods.findByToken = function(cb) {
+userSchema.statics.methods.findByToken = function(cb) {
     var user = this;
 
-    user._id + '' = token
     // 토큰을 decode 한다.
     jwt.verify(token, 'secretToken', function(err, decode) {
         // 유저 아이디를 이용해서 유저를 찾은 다음에
