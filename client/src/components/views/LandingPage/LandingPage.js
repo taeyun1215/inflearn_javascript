4e09ed6723a8e303085c952c9156daadd24bb827
taeyun1215 <taeyun1215@naver.com>
2022-09-14 22:40:47 +0900

    feature/logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 import React, { useEffect } from 'react'
 
-function LandingPage() {
+function LandingPage(props) {
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get('api/hello')
-    .then(response => console.log(response.data))
+      .then(response => {
+        console.log(response.data)
+      })
   }, [])
+
+  const onClickHandler = () => {
+    axios.get('api/users/logout')
+      .then(response => {
+        if (response.data.success) {
+          navigate('/login');
+        } else {
+          alert('로그아웃 하는데 실패 했습니다.')
+        }
+      })
+  }
 
   return (
     <div style={{
@@ -13,6 +30,7 @@ function LandingPage() {
       width: '100%', height: '100vh'
     }}>
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   )
 }
